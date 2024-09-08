43ecd9c5f734a213f2f296b0f9c389dc2e7bd7bb
gilbertoag2007 <gilbertoag2007@gmail.com>
2024-09-07 22:54:12 -0300

    Atualização do projeto

--- script.js
+++ script.js
@@ -2,31 +2,33 @@
 function gerarPlanoDeCorte() {
 
   let campoPreenchido = 1;
- 
+
+  // recupera os valores digitados
   const largura = document.getElementById("largura").value;
-  campoPreenchido = validaCamposObrigatorios("largura",largura);
- 
+  campoPreenchido = validaCamposObrigatorios("largura", largura);
+
   const altura = document.getElementById("altura").value;
   campoPreenchido = validaCamposObrigatorios("altura", altura);
 
   const profundidade = document.getElementById("profundidade").value;
-  campoPreenchido = validaCamposObrigatorios("profundidade",profundidade);
+  campoPreenchido = validaCamposObrigatorios("profundidade", profundidade);
 
   const quantidadePrateleiras = document.getElementById("prateleiras").value;
+  campoPreenchido = validaCamposObrigatorios("prateleira", quantidadePrateleiras);
 
   //Calcula plano de corte apenas se campos obrigatorios estiverem preenchidos.
-  if (campoPreenchido ===1){
-      const modelo = obterModelo();
+  if (campoPreenchido === 1) {
+    const modelo = obterModelo();
 
-      let base = calculaBaseTeto(profundidade, largura);
-      let laterais = calculaLaterais(profundidade, altura);
-      let teto = calculaBaseTeto(profundidade, largura);
-      let fundo = calcularDimensoesFundo(altura, largura);
-      let portas = calcularDimensoesPortas(modelo, altura, largura, null);
-      let prateleiras = calculaPrateleiras(profundidade, largura, quantidadePrateleiras);
-      
-      apresentaPlanoCorte(base, laterais, teto, fundo, portas, prateleiras);
+    let base = calculaBaseTeto(profundidade, largura);
+    let laterais = calculaLaterais(profundidade, altura);
+    let teto = calculaBaseTeto(profundidade, largura);
+    let fundo = calcularDimensoesFundo(altura, largura);
+    let portas = calcularDimensoesPortas(modelo, altura, largura);
+    let prateleiras = calculaPrateleiras(profundidade, largura, quantidadePrateleiras);
 
+    apresentaPlanoCorte(base, laterais, teto, fundo, portas, prateleiras);
+    apresentaImagem(altura, largura, profundidade);
   }
 }
 
@@ -35,39 +37,37 @@ function validaCamposObrigatorios(nome, valor) {
   let preenchido;
 
   if (valor === null) {
- 
-    preenchido= 0;
- 
+    preenchido = 0;
+
   } else if (valor.length < 1) {
- 
-    preenchido= 0;
- 
+    preenchido = 0;
+
   } else {
- 
-    preenchido= 1;
- 
+    preenchido = 1;
+
   }
 
-  if (preenchido === 0){
-    alert ("Preencha o campo "+ nome +".");
+  if (preenchido === 0) {
+    alert("Preencha o campo " + nome + ".");
   }
- return preenchido;
+  
+  return preenchido;
 }
 
 // Calcula a quantidade de portas e respectivas dimensoes
-function calcularDimensoesPortas(modelo, altura, largura, profundidade) {
+function calcularDimensoesPortas(modelo, altura, largura) {
 
-  if (modelo == 1) {// Armario com duas portas e sem gavetas
+  if (modelo == 1) {// Armário multiuso simples
 
     let alturaPorta = (altura - 1);
     let larguraPorta = (largura - 1) / 2
     let observacaoPortas = "Redução de 1 cm na altura e largura das portas para melhor acabamento.";
-
+    
     return formatarResultadoQuantidadePecas(2, 15, alturaPorta, larguraPorta, observacaoPortas);
-  
+
   } else {
 
-    return 'Modelo 2 - Não disponível';
+    return 'Modelo  - Não disponível';
 
   }
 
@@ -78,28 +78,31 @@ function calcularDimensoesFundo(altura, largura) {
 
   let alturaFundo = altura - 1;
   let larguraFundo = largura - 1;
-  let dimensaoFundo = formatarResultadoQuantidadePecas(1, 6, alturaFundo, larguraFundo, null);
+  let observacaoFundo = "Redução de 1 cm na altura e largura para melhor acabamento."
+  let dimensaoFundo = formatarResultadoQuantidadePecas(1, 6, alturaFundo, larguraFundo, observacaoFundo);
   return dimensaoFundo;
 
 }
 // Calcula a dimensao da base e teto
 function calculaBaseTeto(profundidade, largura) {
   
-  let dimensaoBase = formatarResultadoQuantidadePecas(1, 6, profundidade, largura, null);
+  let dimensaoBase = formatarResultadoQuantidadePecas(1, 15, profundidade, largura, null);
   return dimensaoBase;
 
 }
 
 // Calcula a dimensao das pecas laterais do movel
 function calculaLaterais(profundidade, altura) {
-  let dimensaoLateral = formatarResultadoQuantidadePecas(2, 15, altura, profundidade, null);
+  
+  let alturaCalculada = altura - 3;// Descontado 3cm para conta da base e teto de 1,5cm
+  let dimensaoLateral = formatarResultadoQuantidadePecas(2, 15, alturaCalculada, profundidade, null);
   return dimensaoLateral;
-  ;
+  
 }
 
 function calculaPrateleiras(profundidade, largura, quantidadePrateleiras) {
 
-  let larguraRecalculada = (largura -3);// Descontado 3cm para conta de 2 laterais de 1,5cm
+  let larguraRecalculada = (largura - 3);// Descontado 3cm para conta de 2 laterais de 1,5cm
   let dimensaoBase = formatarResultadoQuantidadePecas(quantidadePrateleiras, 15, profundidade, larguraRecalculada, null);
 
   return dimensaoBase;
@@ -113,10 +116,13 @@ function formatarResultadoQuantidadePecas(quantidadePecas, espessura, altura, la
   let observacaoFormatada;
 
   if (quantidadePecas == 1) {
-    pecasFormatada = "<p> 1 peça de mdf(" + espessura + "mm) de " + altura + "cm x " + largura + "cm. </p>";
+    pecasFormatada = "<li> 1 peça de mdf(" + espessura + "mm) de " + altura + "cm x " + largura + "cm. </li>";
+  
   } else {
-    pecasFormatada = "<p>" + quantidadePecas + "peças de mdf(" + espessura + ")mm de " + altura + "cm x " + largura + "cm. </p>";
+    pecasFormatada = "<li>" + quantidadePecas + " peças de mdf(" + espessura + ")mm de " + altura + "cm x " + largura + "cm. </li>";
+  
   }
+  
   resultado = pecasFormatada;
 
   if (observacao != null) {
@@ -125,36 +131,72 @@ function formatarResultadoQuantidadePecas(quantidadePecas, espessura, altura, la
       observacaoFormatada = "<p><strong>Observação:</strong>" + observacao + "</p>";
       resultado = resultado + observacaoFormatada;
     }
+  
   }
 
   return resultado;
 
 }
-
+// Apresenta o plano de corte na tela 
 function apresentaPlanoCorte(base, laterais, teto, fundo, portas, prateleiras) {
+  
   let divResultado = document.getElementById("resultado-plano-corte");
- 
   let resultadoFormatado = "<h2>Plano de Corte</h2>"
     + "<h3>Base</h3>"
+    + "<ul>"
     + base
+    + "</ul>"
 
     + "<h3>Laterias</h3>"
+    + "<ul>"
     + laterais
+    + "</ul>"
 
     + "<h3>Teto</h3>"
+    + "<ul>"
     + teto
+    + "</ul>"
 
     + "<h3>Fundo</h3>"
+    + "<ul>"
     + fundo
+    + "</ul>"
 
     + "<h3>Portas</h3>"
+    + "<ul>"
     + portas
+    + "</ul>"
 
     + "<h3>Prateleiras</h3>"
+    + "<ul>"
     + prateleiras
-
+    + "</ul>"
+    + "<br>"
 
   divResultado.innerHTML = resultadoFormatado;
+}
+
+// Gera imagem do movel com a altura, largura e profundidade informada.
+function apresentaImagem(altura, largura, profundidade) {
+  let divContainerImagem = document.getElementById("container-imagem");
+
+  let resultado = '<hr>'
+
+    + '<center><p>Imagem meramenta ilustrativa. </p></center>'
+    + '<center>'
+    + '<div class="imagem_modelo1">'
+    + '<div class="altura"> A= ' + altura + ' cm</div>'
+    + '<div class="largura"> L= ' + largura + ' cm</div>'
+    + ' <div class="profundidade"> P= ' + profundidade + ' cm</div>';
+  + '</div>'
+    + '</center>'
+
+    ;
+    
+    divContainerImagem.innerHTML = resultado;
+
+  return divContainerImagem
+
 }
 
 //recupera o modelo informado na pagina
